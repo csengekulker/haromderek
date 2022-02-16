57d3178ba0e7e64a65c2f2cfde33f76136d296ac
csengekulker <k71608883429@ktch.hu>
2022-02-16 16:21:28 +0100

    init

---
+++ Haromderek.java
@@ -0,0 +1,17 @@
+/*
+* File: Haromderek.java
+* Author: Balogh Csenge
+* Copyright: 2022, Balogh Csenge
+* Group: Szoft_I_N
+* Date: 2022-02-16
+* Github: https://github.com/csengekulker/
+* Licenc: GNU GPL
+*/
+
+import java.util.Scanner;
+
+class Haromderek {
+    public static void main(String[] args) {
+
+    }
+}
