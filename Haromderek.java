7713c7e6ad4c7e3f0c95848be3c1ec7ed7764900
csengekulker <k71608883429@ktch.hu>
2022-02-16 16:26:55 +0100

    main script done

--- Haromderek.java
+++ Haromderek.java
@@ -12,6 +12,31 @@ import java.util.Scanner;
 
 class Haromderek {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("Balogh Csenge, Szoft_I_N, 2022.02.16");
+        System.out.println("Haromszogvizsgalo");
+
+        System.out.print("A oldal > ");
+        double a = sc.nextDouble();
+        System.out.print("B oldal > ");
+        double b = sc.nextDouble();
+        System.out.print("C oldal > ");
+        double c = sc.nextDouble();
+
+        sc.close();
+
+        if (
+            a + b > c &&
+            a + c > b &&
+            b + c > a
+        ) {
+            System.out.println("A haromszog szerkesztheto.");
+            if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) {
+                System.out.println("A haromszog derekszogu.");
+            }
+        } else {
+            System.out.println("A haromszog nem letezik.");
+        }
     }
 }
